094d428ea500657ac5700a6c9057cd3e89256cd2
aly-abdelkader-iut-cse-4301 <38514887+aly-abdelkader-iut-cse-4301@users.noreply.github.com>
2020-12-02 18:53:55 +0600

    adding navigation

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import DeckList from './components/DeckList';
+import AddDeck from './components/AddDeck';
+import { NavigationContainer, TabActions } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { purple, white } from './utils/colors';
+import { Ionicons, FontAwesome } from "expo";
+
+const RouteConfigs = {
+  DeckList:{
+    name: "DeckList",
+    component: DeckList,
+    options: {tabBarIcon: ({tintColor}) => <Ionicons name='cards' size={30} color={tintColor} />, title: 'DeckList'}
+  }, 
+  AddDeck:{
+    component: AddDeck,
+    name: "AddDeck",
+    options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />, title: 'AddDeck'}
+  }
+}
+
+const Tab = Platform.OS === 'ios'? createBottomTabNavigator() : createMaterialTopTabNavigator()
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === "ios" ? white : purple,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+  };
 
 export default function App() {
   return (
-    <View style={styles.container} >
-     <DeckList />
-    </View>
+    <NavigationContainer>
+            <Tab.Navigator {...TabNavigatorConfig}>
+                <Tab.Screen {...RouteConfigs['DeckList']} />
+                <Tab.Screen {...RouteConfigs['AddDeck']} />
+            </Tab.Navigator>
+        </NavigationContainer>
   );
 }
 

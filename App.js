a4f4db268fa705ac3f624ae880bcfd5e82c2b0e2
aly-abdelkader-iut-cse-4301 <38514887+aly-abdelkader-iut-cse-4301@users.noreply.github.com>
2020-12-02 09:49:43 +0600

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DeckList from './components/DeckList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <DeckList />
     </View>
   );
 }
---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React  from 'react'
+import {Stylesheet,Text,View} from 'react-native'
+
+ class DeckList extends React.Component {
+    render() {
+        return (
+            <div>
+                <view><Text> this is a view text</Text></view>
+            </div>
+        )
+    }
+}
+
+export default DeckList
